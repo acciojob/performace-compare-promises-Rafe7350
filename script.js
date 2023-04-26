bbfa7ebd2ae28620f89d8fc229abd329bd66ec8c
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-04-26 23:01:37 +0530

    add

--- script.js
+++ script.js
@@ -13,3 +13,36 @@ const apiUrls = [
 ];
 
 // You can write your code here
+// Array of API URLs to fetch da
+const fetchData = async (url) => {
+  const startTime = performance.now();
+  await fetch(url);
+  const endTime = performance.now();
+  return endTime - startTime;
+};
+
+// Function to compare performance of Promise.all and Promise.any
+const comparePerformance = async () => {
+  try {
+    const allStartTime = performance.now();
+    await Promise.all(apiUrls.map(url => fetchData(url)));
+    const allEndTime = performance.now();
+    const allTimeTaken = allEndTime - allStartTime;
+    document.getElementById('output-all').textContent = allTimeTaken.toFixed(2);
+  } catch (error) {
+    document.getElementById('output-all').textContent = 'Error';
+  }
+
+  try {
+    const anyStartTime = performance.now();
+    await Promise.any(apiUrls.map(url => fetchData(url)));
+    const anyEndTime = performance.now();
+    const anyTimeTaken = anyEndTime - anyStartTime;
+    document.getElementById('output-any').textContent = anyTimeTaken.toFixed(2);
+  } catch (error) {
+    document.getElementById('output-any').textContent = 'Error';
+  }
+};
+
+// Call the function to compare performance on page load
+window.onload = comparePerformance;
